a4c305f519ea2b0f2ea83b5182e624af44d2e6a6
upingshen <upingshen@gmail.com>
2021-12-18 00:24:13 +0800

    h5

--- another/src/js/pc-index.js
+++ another/src/js/pc-index.js
@@ -1,6 +1,14 @@
 ﻿// swiper
 var busSwiper = new Swiper("#bus-swiper-container", {
-  slidesPerView: 3.5,
+  slidesPerView: 2.5,
+  spaceBetween: 15,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+new Swiper("#centification-swiper-container", {
+  slidesPerView: 5,
   spaceBetween: 15,
   navigation: {
     nextEl: ".swiper-button-next",
