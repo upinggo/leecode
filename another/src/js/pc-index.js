f166257a849ac6e806e4b3523acef0dce68316c5
Neil SHEN <neil.shen@sap.com>
2021-12-17 20:06:52 +0800

    up

--- another/src/js/pc-index.js
+++ another/src/js/pc-index.js
@@ -1,1 +1,9 @@
-﻿
+﻿// swiper
+var busSwiper = new Swiper("#bus-swiper-container", {
+  slidesPerView: 3.5,
+  spaceBetween: 15,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
